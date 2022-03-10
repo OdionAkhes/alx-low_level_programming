63e47e7b0576ec33db4740c7d9045fc1041b3252
Akhetuamhen Odion <akhetuamhenodion@gmail.com>
2022-03-10 07:03:56 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - prints a sentence followed by a new line
- *
- * Return: returns 1 after printing
-*/
-
+ * main - write to stdout
+ * Return: Always 0
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
