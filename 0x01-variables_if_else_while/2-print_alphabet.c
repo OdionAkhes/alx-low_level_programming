da3d51d8bc9a4687b7e2337d56b323fbe5000920
Akhetuamhen Odion <akhetuamhenodion@gmail.com>
2022-03-11 04:10:31 -0800

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- *
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-int i;
-
-for (i = 0; i < 26; i++)
-{
-putchar(alp[i]);
-}
-putchar(n);
-return (0);
+	 char ch; for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
