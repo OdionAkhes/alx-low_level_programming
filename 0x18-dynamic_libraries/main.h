540bc79e40ea38f600612b3f11298be17138a29f
Akhetuamhen Odion <akhetuamhenodion@gmail.com>
2022-06-13 11:57:18 -0700

    library

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <stdlib.h>
+#ifndef _MAIN_
+#define _MAIN_
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBERTON_H */
+#endif
