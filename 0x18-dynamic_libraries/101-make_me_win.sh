22746de36c21aacd463d5f5bfdf4a11b0b2d8bd6
Akhetuamhen Odion <akhetuamhenodion@gmail.com>
2022-06-13 13:17:29 -0700

    '

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/bash                                                               
+#!/bin/bash
 wget -P /tmp https://github.com/OdionAkhes/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
